bafd6d3590c4b50b9c7530b97f6c7a54639c92ba
lucalmr <lucalmr12@gmail.com>
2023-06-29 20:20:01 -0300

    Adicionar arquivos de projeto.

---
+++ VETOR OBJETO CLASSE/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VETOR_OBJETO_CLASSE {
+    internal class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ VETOR OBJETO CLASSE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VETOR_OBJETO_CLASSE {
+    internal class Program {
+        static void Main(string[] args) {
+            int N = int.Parse(Console.ReadLine());
+
+            Product[] Vetor = new Product[N];
+            for (int i = 0; i < N; i++) {
+
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Vetor[i] = new Product { Name = nome, Price = preco };
+
+            }
+            double soma = 0.0;
+
+            for (int i = 0; i < N; i++) {
+
+                soma += Vetor[i].Price;
+
+            }
+            double mediaFinal = soma / N;
+            Console.WriteLine("Preço Medio = " + mediaFinal.ToString("F2",CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        } 
+    }
+}
